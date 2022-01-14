d563022afa7f80b76ff68ad6e622a4be276ab6de
s4hil <s4hilp4rr4y@gmail.com>
2022-01-14 10:32:23 +0530

    user id bug

--- assets/phpStuff/fetchSubmission.php
+++ assets/phpStuff/fetchSubmission.php
@@ -20,8 +20,7 @@
 
 	$data = json_decode(file_get_contents("php://input"), true);
 
-	// $user_id = clean($data['user_id']);
-	$user_id = 8959;
+	$user_id = clean($data['user_id']);
 	$user_email = fetchEmailById($user_id);
 
 	$sql = "SELECT * FROM `_submissions` WHERE `user_id` = '$user_id' LIMIT 1";
